d04685233991bebedc0423b9a6c7cfbade70938b
Aelwin <juanlopez633@gmail.com>
2015-07-14 19:55:04 +0200

    lista de preguntas

--- models/models.js
+++ models/models.js
@@ -37,10 +37,11 @@ sequelize.sync().then(function() {
 	//then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count) {
 		if (count === 0) { //la tabla se inicializa solo si esta vacia
-			Quiz.create({pregunta: 'Capita de Italia',
-				respuesta: 'Roma'}).then(function() {
+			Quiz.create({pregunta: 'Capita de Italia', respuesta: 'Roma'});
+			Quiz.create({pregunta: 'Capita de Portugal',
+				respuesta: 'Lisboa'}).then(function() {
 					console.log('Base de datos inicilizada');
-				});
+				});				
 		}
 	});
 });
